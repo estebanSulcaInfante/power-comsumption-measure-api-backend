44e13673b31e6185d9bfd8edfe53d6b9bee6146e
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2025-05-25 01:10:52 -0500

    Prep for Render deployment fixed port

--- api_flask.py
+++ api_flask.py
@@ -141,6 +141,6 @@ def get_energy_stats():
     return jsonify(stats), 200
 
 
-if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
